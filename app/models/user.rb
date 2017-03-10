ed1149315b519462cc5b64068d6910303f140b1f
jordan beth <jordanbeth14@gmail.com>
2017-03-10 12:15:27 -0500

    created models and schema for users, posts, likes

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :likes
+  has_many :post_likes, through: :likes, source: :post
+
+  validates :username, :email, presence: true, uniqueness: true
+
+  has_secure_password
+end
