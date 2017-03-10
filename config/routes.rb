fc2314de7173a038070d5d0fce9d6ff6f130fa5b
jordan beth <jordanbeth14@gmail.com>
2017-03-10 15:25:06 -0500

    dded routes efor posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
 
+  resources :posts, only: [:new, :create, :edit, :delete]
+  
   resources :users, only: [:show, :new, :create]
 
 end
